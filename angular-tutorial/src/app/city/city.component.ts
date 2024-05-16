9f9d3fb298541fc8305686890c219bf883246c2c
ChienBV <chienbv@aratech.vn>
2024-05-16 11:53:22 +0700

    ng-content + typeScript

--- angular-tutorial/src/app/city/city.component.ts
+++ angular-tutorial/src/app/city/city.component.ts
@@ -2,11 +2,12 @@ import { CityDto } from './../models/CityDto';
 import { Component } from '@angular/core';
 import { CityDetailComponent } from '../city-detail/city-detail.component';
 import { NgFor } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-city',
   standalone: true,
-  imports: [CityDetailComponent, NgFor],
+  imports: [CityDetailComponent, NgFor, MatIconModule],
   templateUrl: './city.component.html',
   styleUrl: './city.component.scss',
 })
@@ -34,5 +35,14 @@ export class CityComponent {
 
   handleAddCity(city: any) {
     this.cities.push(city);
+  }
+
+  handleUpdateCities() {
+    this.cities.push({
+      id: 89,
+      name: 'Test',
+      address: 'test',
+      urlPicture: '12234',
+    });
   }
 }
